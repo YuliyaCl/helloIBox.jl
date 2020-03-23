c9b46723561143da0cc613dd82eaf46cc56b588f
YuliyaCl <36585857+YuliyaCl@users.noreply.github.com>
2020-03-23 14:51:03 +0300

    Files generated by PkgTemplates

---
+++ src/helloIBox.jl
@@ -0,0 +1,5 @@
+module helloIBox
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using helloIBox
+using Test
+
+@testset "helloIBox.jl" begin
+    # Write your own tests here.
+end
